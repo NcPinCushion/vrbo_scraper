eb3226af84c0297319f4f6ec9ff49058fb47bd63
Joshua P. Shindelbower <jps582@humboldt.edu>
2017-04-17 10:22:48 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 import scraperwiki
 from pyquery import PyQuery
 
-url = 'http://www.vrbo.com/373926'
+url = 'https://www.vrbo.com/vacation-rentals?swLat=41.02271946788004&swLong=-124.16536826990239&neLat=41.108011129393624&neLong=-124.0891506185352&zoom=13&page=1&region=2179&searchTermContext=b1134c31-65a3-4128-bb00-74656f864008'
 
 q = PyQuery(url)
 
